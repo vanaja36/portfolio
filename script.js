b0f0141863d78cb7feacacdd088b221faaf4b20c
vanaja36 <168762188+vanaja36@users.noreply.github.com>
2024-06-25 20:47:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".multiple-text",{
+    string: ["frontend developer ", "backend developer"," graphic designer"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+})
